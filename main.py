3d379d311b1f2e1ed993ac8b72ab81ed0fdeb1eb
Anish Soni <anish.21cse189@rtu.ac.in>
2024-03-27 02:15:39 +0530

    initialised streamlit for frontend

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello world~")
+# Import streamlit for app dev
+import streamlit as st
+
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
+import torch
+from llama_index.prompts.prompts import SimpleInputPrompt
+from llama_index.llms import HuggingFaceLLM
+from llama_index.embeddings import LangchainEmbedding
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from llama_index import set_global_service_context
+from llama_index import ServiceContext
+from llama_index import VectorStoreIndex, download_loader
+from pathlib import Path
